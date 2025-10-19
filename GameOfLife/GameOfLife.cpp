242f312eea18e7956c2ca95af5f36c18a3e92fab
Dim <Dim@DIMPC>
2025-10-20 00:02:26 +0300

    fix

--- GameOfLife/GameOfLife.cpp
+++ GameOfLife/GameOfLife.cpp
@@ -117,7 +117,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             }
         }
 
-        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
         structure.UpdateMap();
 
